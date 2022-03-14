a38bf3d7ad526c2380265e5b56a0efb3f2bd8b8c
Dean_Zhong <99632732+Deanpraisethesun@users.noreply.github.com>
2022-03-14 11:00:51 +0800

    Update RDP_PortChange_reboot.ps1

--- RDP_PortChange_reboot.ps1
+++ RDP_PortChange_reboot.ps1
@@ -3,8 +3,8 @@
 #設定key值，並將對應的rule 加入firewall
 $NewPortNum = 13579
 Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "PortNumber" -Value $NewPortNum
-New-NetFirewallRule -DisplayName 'RDP_PORT_TCPIn' -Profile any -Direction Inbound -Action Allow -Protocol TCP -LocalPort $NewPortNum
-New-NetFirewallRule -DisplayName 'RDP_PORT_UDPIn' -Profile any -Direction Inbound -Action Allow -Protocol UDP -LocalPort $NewPortNum
+New-NetFirewallRule -DisplayName "RDP_PORT_TCPIn" -Profile any -Direction Inbound -Action Allow -Protocol TCP -LocalPort $NewPortNum
+New-NetFirewallRule -DisplayName "RDP_PORT_UDPIn" -Profile any -Direction Inbound -Action Allow -Protocol UDP -LocalPort $NewPortNum
 
 #重啟套用變更
 Restart-Computer -Force
